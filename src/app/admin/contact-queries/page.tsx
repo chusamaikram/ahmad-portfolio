5e1d13aaa92e6ed9b7d21215a7d3bfa0e89b2e07
chusamaikram <usamachaudhary0345@gmail.com>
2026-04-20 17:30:18 +0500

    updated

--- src/app/admin/contact-queries/page.tsx
+++ src/app/admin/contact-queries/page.tsx
@@ -1,5 +1,12 @@
+import { Suspense } from "react";
 import AdminContactQueriesView from "@/src/components/features/admin/contact-queries";
 
+export const dynamic = "force-dynamic";
+
 export default function AdminContactQueriesPage() {
-  return <AdminContactQueriesView />;
+  return (
+    <Suspense>
+      <AdminContactQueriesView />
+    </Suspense>
+  );
 }
